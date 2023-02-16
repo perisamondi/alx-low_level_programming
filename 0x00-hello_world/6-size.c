0578bd7d605f80e4d332b6a6fded55a185958f5c
perisamondi <perisamondi@gmail.com>
2023-02-16 07:29:37 -0800

    code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ *
+ * Return: Always 0
+ *\
+
+ int main(void)
+ { 
+       char a;
+       int b;
+       long int c;
+       long long int d;
+       float e;
+
+ printf("Size of char: %lu byte(s)\n" ,(unassigned long)size of(a));
+ printf("Size of int: %lu byte(s)\n" ,(unsigned long)sizeof(b));
+ printf("Size of long int : %lu byte(s)\n" ,(unsigned long)sizeof(c));
+ printf("Size of long long int: %lu byte(s)\n" ,(unsigned long)sizeof(d));
+ printf("Size of float : %lu byte(s)\n" ,(unsigned long)sizeof(e));
+ return (0);
+ }
