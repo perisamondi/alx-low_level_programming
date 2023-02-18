9c31aa5f7ae7b2023005f6a24b258a7853de4a99
perisamondi <perisamondi@gmail.com>
2023-02-18 01:28:21 -0800

    code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include <unistd.c>
 /**
  * main - Entry point
  * 
  * Return: Always 1
  */
-int  main(void)
-{ 
-	write(2"and thatpiece of art is useful-Dora Korpar, 2015-10-19\n");
-	return(1);
+int main(void)
+{
+fputs("and thatpiece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+return(1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {    
-     printf("Size of char: %i byte(s)\n", sizeof(char));
- printf("Size of int: %i byte(s)\n", sizeof(iZnt));
- printf("Size of long int: %i byte(s)\n", sizeof(longint);
- printf("Size of long long int: %i byte(s)\n", sizeof(longlongint));
- printf("Size of float: %i byte(s)\n", sizeof(float));
- return (0);
+printf("Size of char: %i byte(s)\n", sizeof(char));
+printf("Size of int: %i byte(s)\n", sizeof(int));
+printf("Size of long int: %i byte(s)\n", sizeof(long);
+printf("Size of long long int: %i byte(s)\n", sizeof(long long));
+printf("Size of float: %i byte(s)\n", sizeof(float));
+return (0);
 }
