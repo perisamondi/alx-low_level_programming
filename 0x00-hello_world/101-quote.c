0ecf944936ceee6468870997f4fcc8b1770bdfdf
perisamondi <perisamondi@gmail.com>
2023-02-17 23:46:46 -0800

    code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.c>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 1
+ */
+int  main(void)
+{ 
+	write(2"and thatpiece of art is useful-Dora Korpar, 2015-10-19\n");
+	return(1);
+}
