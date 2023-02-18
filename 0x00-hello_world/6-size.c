fd0fb3865c3709c2d6239e5c6c490f2e4362d9da
perisamondi <perisamondi@gmail.com>
2023-02-17 23:27:07 -0800

    code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,10 @@
  * main -Entry
  *
  * Return: Always 0
- *\
+ */
  int main(void)
  {      
- printf("Size of char: %i byte(s)\n"; sizeof (char));
+ printf("Size of char: %i byte(s)\n"; sizeof(char));
  printf("Size of int: %i byte(s)\n"; sizeof(int));
  printf("Size of long int: %i byte(s)\n"; sizeof(longint));
  printf("Size of long long int: %i byte(s)\n"; sizeof(longlongint));
