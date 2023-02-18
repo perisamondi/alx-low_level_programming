e3a14071bc0d01441f5cf45fb4cab04ce177b579
perisamondi <perisamondi@gmail.com>
2023-02-17 23:33:37 -0800

    code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,12 @@
  *
  * Return: Always 0
  */
- int main(void)
- {      
- printf("Size of char: %i byte(s)\n"; sizeof(char));
- printf("Size of int: %i byte(s)\n"; sizeof(int));
- printf("Size of long int: %i byte(s)\n"; sizeof(longint));
- printf("Size of long long int: %i byte(s)\n"; sizeof(longlongint));
- printf("Size of float: %i byte(s)\n"; sizeof(float));
+int main(void)
+{      
+ printf("Size of char: %i byte(s)\n"); sizeof(char);
+ printf("Size of int: %i byte(s)\n"); sizeof(int);
+ printf("Size of long int: %i byte(s)\n"); sizeof(longint);
+ printf("Size of long long int: %i byte(s)\n"); sizeof(longlongint);
+ printf("Size of float: %i byte(s)\n"); sizeof(float);
  return (0);
- }
+}
