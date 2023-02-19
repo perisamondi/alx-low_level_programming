5df2db7ec07d9b4c31574b2b74c1377756e42140
perisamondi <perisamondi@gmail.com>
2023-02-19 03:02:41 -0800

    cvariables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int  main(void)
+{
+	int n;
+	int a;
+
+	rand(time(0));
+        n = rand() - RAND_MAX / 2;
+        
+        return (0);
