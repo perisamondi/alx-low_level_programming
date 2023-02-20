ce947eb0fb1fe29d2efc7909358e6a4e101726fb
perisamondi <perisamondi@gmail.com>
2023-02-20 06:39:37 -0800

    cvar

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,11 +12,12 @@ int main(void)
 	char c;
 
 	c = 'a';
+
 	while
-		( c = 'a') {
-			if (( c != 'e' && c != 'q') && c <= 'z');
-			putchar(c);
-			c++;
+		(c <= 'z'){
+		if (( c != 'e' && c != 'q') && c <= 'z')
+		putchar(c);
+		c++;
 		}
 	putchar('\n');
 	return (0);
