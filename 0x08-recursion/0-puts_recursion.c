df91c5510360d7dc3c9d9a6276e722f9946591c9
perisamondi <perisamondi@gmail.com>
2023-03-08 23:36:23 -0800

    printf

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include  "main.h"
+
+/**
+ * printf- prints out a string
+ * @s: input
+ * @\n: input
+ * Return 0 on success
+ */
+void _puts_recursion(char *s)
+{
+	printf();
+	 char s;
+	 int i = 0;
+	 char *s = printf(char s[i]);
+	 
+	 putchar(char *s);
+	 putchr('\n');
+	 return (0);
+}
