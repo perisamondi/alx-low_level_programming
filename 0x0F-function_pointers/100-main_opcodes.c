d62f0e38f9002ca00ffb36f3c0b53e9502d77aed
perisamondi <perisamondi@gmail.com>
2023-03-22 12:40:18 -0700

    fpointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,40 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * print_opcodes-  prints the opcodes
+ * @a : address of main  function
+ * @n : number of bytes to print
+ * Return: void
+ */
+void print_ocodes(char *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+        {
+         	printf("%.2hhx\n", a[i]);
+
+		if (i < n  - 1)
+			printf(" ");
+        }
+	printf(" \n");
+}
+/**
  * main-  prints the opcodes
  * @argc: number of arguments in a program
  * @argv: array of arguments
  * Return: Always 0
  */
-int main(int argc , char *argv[])
+int main(int argc, char **argv)
 {
-	int bytes;
-	int i;
-	char *arr;
-
+	int n;
+	
 	if (argc != 2)
-        {
-                printf("Error\n");
-                exit(1);
-        }
-        bytes = atoi(argv[1]);
-
-        if (bytes < 0)
-        {
-                printf("Error\n");
-                exit(2);
-        }
-
-        arr = (char *)main;
-
-        for(i = 0; i < bytes; i++)
-        {
-                if (i == bytes -1)
-                {
-                        printf("%02hhx\n", arr[i]);
-                        break;
-                }
-                printf("%02hhx",arr[i]);
-        }
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opocodes((char *)&main, n);
 	return (0);
 }
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,8 +21,11 @@ int (*get_op_func(char *s))(int, int)
 	 int i;
 	 
 	 i = 0;
-
-    while ((ops[i].op != NULL && *(ops[i].op != s))
-		    i++;
-		    return (ops[i].f);
+	 while (i < 5)
+	 {
+		i (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	 }
+	 return (0);
 }
